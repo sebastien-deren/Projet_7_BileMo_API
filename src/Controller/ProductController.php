3c690c58a641b52d9c4ef29790b44063f05b5e67
sebastien deren laptop <sebastien.deren@gmail.com>
2023-05-24 11:49:33 +0200

    add HTTPcache

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
@@ -23,6 +24,7 @@ class ProductController extends AbstractController
      * @throws InvalidArgumentException
      * @throws \Exception
      */
+    #[Cache(maxage:3600,public:false,mustrevalidate:true)]
     #[Route('products/', name: 'app_product_list', methods: 'get')]
     public function list(Request      $request,
                          ProductService $productService): JsonResponse
