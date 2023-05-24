9f1ec314d82e59dd9375ad1e35c4ecc0bf77e8a6
sebastien deren laptop <sebastien.deren@gmail.com>
2023-05-24 11:48:17 +0200

    creation of the cache

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,25 +8,25 @@ use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 class UserController extends AbstractController
 {
+    #[Cache(maxage: 60, public: false, mustRevalidate: true)]
     #[Route('client/{clientId}/users/{id/+}', name: "app_user_detail", methods: "GET")]
     public function detail(
-        int               $id,
-        int               $clientId,
-        UserService       $service,
-        ClientService     $clientService,
-        serializerService $serializer) : JsonResponse
+        int           $id,
+        int           $clientId,
+        UserService   $service,
+        ClientService $clientService
+    ): JsonResponse
     {
 
         $client = $clientService->getValidClient($this->getUser(), $clientId);
         $user = $service->getValidUser($id, $client);
         $jsonRepsonse = $service->detailJsonResponse($user);
-        //how to cache this without SensioFrameworkbundle to secure our route. We need to not use the HTTPCache but to actually hit our app to verify authentication first!
-
         return $jsonRepsonse;
     }
 }
