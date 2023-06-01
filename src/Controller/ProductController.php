f73b0a84fc0935ed8b8f8699be6be3c0d15d864d
sebastien deren laptop <sebastien.deren@gmail.com>
2023-06-01 10:02:21 +0200

    correcting typo in HTTPcache

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use App\Service\Headers\PaginationHeaderInterface;
 use App\Service\ProductService;
 use App\Service\SerializerService;
-use http\Exception\BadQueryStringException;
-use PHPUnit\Util\Exception;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -14,8 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\ItemInterface;
-use Symfony\Contracts\Cache\TagAwareCacheInterface;
+
 
 #[Route('/api/')]
 class ProductController extends AbstractController
@@ -24,7 +21,7 @@ class ProductController extends AbstractController
      * @throws InvalidArgumentException
      * @throws \Exception
      */
-    #[Cache(maxage: 3600, public: false, mustrevalidate: true)]
+    #[Cache(maxage: 3600, public: false, mustRevalidate: true)]
     #[Route('products', name: 'app_product_list', methods: 'get')]
     public function list(Request           $request,
                          ProductService    $productService,
