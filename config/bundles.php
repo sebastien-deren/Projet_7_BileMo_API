c6d3b1e5338e32d23ddc8a61e39aa8e336b42cac
sebastien deren laptop <sebastien.deren@gmail.com>
2023-06-12 14:26:34 +0200

    adding twig for our documentation

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
